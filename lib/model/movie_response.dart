f246bbfbca359bf527d78322048e0d1ce2df2fa1
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-06-30 16:48:10 -0400

    created genre model and created repository

--- lib/model/movie_response.dart
+++ lib/model/movie_response.dart
@@ -1,18 +1,18 @@
 import 'package:movie_app/model/movie.dart';
 
-class MovieRespose {
+class MovieResponse {
   final List<Movie> movies;
   final String error;
 
-  MovieRespose(this.movies, this.error);
+  MovieResponse(this.movies, this.error);
 
-  MovieRespose.fromJson(Map<String, dynamic> json)
+  MovieResponse.fromJson(Map<String, dynamic> json)
       : movies = (json['results'] as List)
             .map((i) => new Movie.fromJson(i))
             .toList(),
         error = '';
 
-  MovieRespose.withError(String errorValue)
+  MovieResponse.withError(String errorValue)
       : movies = List(),
         error = errorValue;
 }
