4b8d0a2278bd628dfd1aec2721cdc4c013fb799b
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-06-30 17:24:51 -0400

    Implemented BLoC pattern

---
+++ lib/bloc/get_persons_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:movie_app/model/person_response.dart';
+import 'package:movie_app/repository/repository.dart';
+import 'package:rxdart/rxdart.dart';
+
+class PersonsListBLoc {
+  final MovieRepository _repository = MovieRepository();
+  final BehaviorSubject<PersonRespose> _subject =
+      BehaviorSubject<PersonRespose>();
+
+  getMovies() async {
+    PersonRespose response = await _repository.getPersons();
+    _subject.sink.add(response);
+  }
+
+  dispose() {
+    _subject.close();
+  }
+
+  BehaviorSubject<PersonRespose> get subject => _subject;
+}
+
+final moviesByGenre = PersonsListBLoc();
